460b45874c791ea89481acd8efeff5e2336c2418
git--yasith02 <yasithuvanpriya@gmail.com>
2025-04-22 18:58:44 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,59 +13,28 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "space app",
       home: Scaffold(
-        // Remove backgroundColor from Scaffold
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          shadowColor: const Color.fromARGB(255, 244, 243, 239),
-          elevation: 5.0,
-          toolbarHeight: 70,
-          title: const Text(
-            "BLACK HOLE",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 15,
-              color: Colors.white,
-            ),
-          ),
-          actions: [
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.only(bottom: 3),
-              child: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.menu),
-              ),
-            ),
-          ],
-        ),
         body: Container(
+          width: double.infinity, // Makes container stretch full width
+          height: double.infinity, // Makes container stretch full height
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: [Colors.black, Colors.deepPurple],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+              colors: [Color(0xFF060C46), Color(0xFF39193D)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
           ),
           child: Padding(
             padding: const EdgeInsets.all(12.0),
             child: Column(
-              children: [
-                const Text(
-                  "SPACE DETAILS",
+              crossAxisAlignment: CrossAxisAlignment.center, // Align text to left
+              children: const [
+                SizedBox(height: 50),
+                Text(
+                  "Direction",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Center(
-                    child: Image.asset(
-                      "assests/space1.png",
-                      height: 250,
-                      scale: 2,
-                    ),
+                    fontSize: 30,
+                    fontWeight: FontWeight.normal,
                   ),
                 ),
               ],
